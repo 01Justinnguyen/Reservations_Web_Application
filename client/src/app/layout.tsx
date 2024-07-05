bf349371a68ec044e17e9f370ed5c67906d3ad9a
Liam <phuc01112002@gmail.com>
2024-07-05 21:03:37 +0700

    #5 - Setup tanstack query and coding feature login

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 import envConfig from '../../config'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/toaster'
+import AppProvider from '@/components/app-provider'
 console.log(envConfig)
 
 const fontSans = FontSans({
@@ -25,10 +26,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
-          <Toaster />
-        </ThemeProvider>
+        <AppProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+            <Toaster />
+          </ThemeProvider>
+        </AppProvider>
       </body>
     </html>
   )
