ea3bb5f52204ea4bc8b14bf5226807ed54cabf19
Liam <phuc01112002@gmail.com>
2024-08-11 10:04:25 +0700

    #134 - Handle catching with ISR technique

--- client/src/apiRequests/dish.ts
+++ client/src/apiRequests/dish.ts
@@ -5,7 +5,7 @@ const prefix = '/dishes'
 const dishApiRequest = {
   // Nếu là Nextjs 15 thì mặc định fetch sẽ là {cache: 'no-store'}
   // Hiện tại Nextjs 14 mặc định fetch sẽ là {cache: 'force-cache'}
-  list: () => http.get<DishListResType>(`${prefix}`),
+  list: () => http.get<DishListResType>(`${prefix}`, { next: { tags: ['dishes'] } }),
   getDish: (id: number) => http.get<DishResType>(`${prefix}/${id}`),
   create: (body: CreateDishBodyType) => http.post<DishResType>(`${prefix}`, body),
   update: (id: number, body: UpdateDishBodyType) => http.put<DishResType>(`${prefix}/${id}`, body),
