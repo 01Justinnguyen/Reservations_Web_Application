5268aeefa69ae858f8b143bf61cf775ab53c057c
Liam <phuc01112002@gmail.com>
2024-08-17 09:47:42 +0700

    #157 - Handle authorization between role manage and guest

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,7 +1,11 @@
+import { Role } from '@/constants/type'
+import { decodedToken } from '@/lib/utils'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const privatePaths = ['/manage']
+const managePaths = ['/manage']
+const guestPaths = ['/guest']
+const privatePaths = [...managePaths, ...guestPaths]
 const unAuthPaths = ['/login']
 
 // This function can be marked `async` if using `await` inside
@@ -17,17 +21,32 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
-  // Đăng nhập rồi mà muốn về trang login thì đá về trang chủ
-  if (unAuthPaths.some((path) => pathname.startsWith(path) && refreshToken)) {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
+  // Trường hợp đã đăng nhập
+  if (refreshToken) {
+    // Nếu cố tình vào trang login sẽ redirect về trang chủ
+    if (unAuthPaths.some((path) => pathname.startsWith(path))) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
 
-  // Trường hơp đăng nhập rồi nhưng access token lại hết hạn
-  if (privatePaths.some((path) => pathname.startsWith(path) && !accessToken && refreshToken)) {
-    const url = new URL('/refresh-token', request.url)
-    url.searchParams.set('refreshToken', refreshToken ?? '')
-    url.searchParams.set('redirect', pathname)
-    return NextResponse.redirect(url)
+    // Trường hơp đăng nhập rồi nhưng access token lại hết hạn
+    if (privatePaths.some((path) => pathname.startsWith(path) && !accessToken)) {
+      const url = new URL('/refresh-token', request.url)
+      url.searchParams.set('refreshToken', refreshToken ?? '')
+      url.searchParams.set('redirect', pathname)
+      return NextResponse.redirect(url)
+    }
+
+    // Nếu truy cập vào không đúng quyền hạn của role thì redirect về trang chủ
+    const role = decodedToken(refreshToken).role
+    // Guest nhưng cố truy cập vào route manage
+    const isGuestGotoManagePath = role === Role.Guest && managePaths.some((path) => path.startsWith(path))
+    // Không phải guest nhưng cố truy cập vào route guest
+    const isNotGuestGoToGuestPath = role !== Role.Guest && guestPaths.some((path) => pathname.startsWith(path))
+    // Không phải Owner nhưng cố tình truy cập vào các route dành cho owner
+
+    if (isGuestGotoManagePath || isNotGuestGoToGuestPath) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
   }
 
   return NextResponse.next()
@@ -35,5 +54,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/manage/:path*', '/login']
+  matcher: ['/manage/:path*', '/guest/:path*', '/login']
 }
